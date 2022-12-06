59cad474676cef04b4df436f37dfb18734fc2dad
Gokulkalkar <110715032+Gokulkalkar@users.noreply.github.com>
2022-12-06 19:05:34 +0530

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,36 @@
+variable "region" {
+  type = string
+}
+variable "profile" {
+  type = string
+}
+
+variable "cidr_block" {
+  type = string
+}
+variable "tags" {
+  type = string
+}
+
+variable "public_cidr_block" {
+  type = string
+}
+
+variable "private_cidr_block" {
+  type = string
+}
+variable "instance_type" {
+  type    = string
+
+}
+variable "key_name" {
+  type = string
+}
+
+variable "ami" {
+  type = string
+}
+/*
+output "public_ip_address" {
+  value = aws_instance.this.public_ip
+}*/
